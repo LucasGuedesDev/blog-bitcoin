23bb33d9041ba5da74dcce76987671e2a983cff3
LucasGuedesDev <guedeslucas033@gmail.com>
2025-04-07 01:30:40 -0300

    Finalizando Projeto - Blog em Angular

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Bitcoin atinge nova máxima histórica",
+    "description": "A criptomoeda ultrapassa os 100 mil dólares e anima investidores.",
+    "photoCover":"https://static.poder360.com.br/2024/02/simbolo-bitcoin-848x477.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Mineradores de Bitcoin migram para energia limpa",
+    "description": "Sustentabilidade ganha força no mercado de criptoativos.",
+    "photoCover":"https://images.pexels.com/photos/534216/pexels-photo-534216.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500=1"
+  },
+  {
+    "id":"3",
+    "title": "Instituições financeiras apostam alto no Bitcoin",
+    "description": "Sustentabilidade ganha força no mercado de criptoativos.",
+    "photoCover":"https://investidoremvalor.com/wp-content/uploads/2016/11/buy-and-hold.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Regulação do Bitcoin avança no Brasil",
+    "description": "Governo discute regras para garantir segurança nas transações.",
+    "photoCover":"https://andrebona.com.br/wp-content/uploads/2016/06/Como_saber_se_o_meu_investimento_est%C3%A1_dando_certo.jpg-640x370.jpeg"
+  }
+]
